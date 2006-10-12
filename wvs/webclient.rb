c2d5dfda0de58179aa714b3c5f8d2a0b6ab5b37f
Tanaka Akira <akr@fsij.org>
2006-10-12 19:13:19 +0000

    move codeblog_handler to wvs/auth.rb.
    git-svn-id: svn://svn.a-k-r.org/akr/wvs/trunk@89 0ecacc03-ac1b-0410-941d-e442c79421a6

--- wvs/webclient.rb
+++ wvs/webclient.rb
@@ -1,6 +1,7 @@
 require 'net/https'
 require 'wvs/form'
 require 'wvs/cookie'
+require 'wvs/auth'
 require 'keyring'
 
 class WVS::WebClient
@@ -25,7 +26,7 @@ class WVS::WebClient
 
   def initialize
     @handler = []
-    @handler << method(:codeblog_handler)
+    @handler << WVS::Auth.method(:codeblog_handler)
 
     @cookies = {}
   end
@@ -43,77 +44,6 @@ class WVS::WebClient
     if !cs.empty?
       req['Cookie'] = cs.map {|(domain, path, name), c| c.encode_cookie_field }.join('; ')
     end
-  end
-
-  def codeblog_handler(uri, req, resp)
-    unless resp.code == '403' &&
-           uri.scheme == 'https' &&
-           uri.host == 'www.codeblog.org' &&
-           uri.port == 443
-      return nil
-    end
-    errpage = resp.body
-    return nil if /Log in via TypeKey/ !~ errpage
-    typekey_uri = nil
-    HTree(errpage).traverse_element("{http://www.w3.org/1999/xhtml}a") {|e|
-      if href = e.get_attr('href')
-        href = URI(href)
-        if href.host == 'www.typekey.com'
-          typekey_uri = href
-          break
-        end
-      end
-    }
-    return nil if !typekey_uri
-    sign_in_form = nil
-    HTree(typekey_uri).traverse_element('{http://www.w3.org/1999/xhtml}form') {|form|
-      form = WVS::Form.make(typekey_uri, form)
-      if form.has?('username') && form.has?('password')
-        sign_in_form = form
-        break
-      end
-    }
-    return nil if !sign_in_form
-    KeyRing.with_authinfo(KeyRing.typekey_protection_domain) {|username, password|
-      sign_in_form.set('username', username)
-      sign_in_form.set('password', password)
-      referer = typekey_uri
-      sign_in_form.make_request(nil) {|req|
-        req["Referer"] = referer.to_s
-        resp = do_request(sign_in_form.action_uri, req)
-      }
-    }
-    # The password vanishing is not perfect, unfortunately.
-    # arr = []; ObjectSpace.each_object(String) {|s| arr << s }; arr.each {|v| p v }
-    return nil if resp.code != '302'
-    codeblog_uri = nil
-    cookies = []
-    resp.canonical_each {|k, v|
-      case k
-      when /\ALocation\z/i
-        codeblog_uri = URI(v)
-      when /\ASet-Cookie\z/i
-        update_cookies(sign_in_form.action_uri, v)
-      end
-    }
-
-    req = Net::HTTP::Get.new(codeblog_uri.request_uri)
-    insert_cookie_header(codeblog_uri, req)
-    resp = do_request(codeblog_uri, req)
-    codeblog_uri2 = nil
-    resp.canonical_each {|k, v|
-      case k
-      when /\ALocation\z/i
-        codeblog_uri2 = URI(v)
-      when /\ASet-Cookie\z/i
-        update_cookies(codeblog_uri, v)
-      end
-    }
-
-    req = Net::HTTP::Get.new(codeblog_uri2.request_uri)
-    insert_cookie_header(codeblog_uri2, req)
-
-    return codeblog_uri2, req
   end
 
   def do_request(uri, req)
@@ -150,7 +80,7 @@ class WVS::WebClient
       break if resp.code == '200'
       r = nil
       @handler.each {|h|
-        if r = h.call(uri, req, resp)
+        if r = h.call(self, uri, req, resp)
           uri, req = r
           break
         end
