a86be8d15685e52628aa1c06812b2c6921259390
Tanaka Akira <akr@fsij.org>
2007-01-01 15:47:54 +0000

    tarball generation system.
    git-svn-id: svn://svn.a-k-r.org/akr/wfo/trunk@223 0ecacc03-ac1b-0410-941d-e442c79421a6

---
+++ make-dist.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+version="`cat VERSION`" &&
+tmpdir="/tmp/wfo-dist-$$" &&
+mkdir $tmpdir &&
+(
+  cd $tmpdir &&
+  svn co svn://svn@rx1620.fsij.org/akr/wfo/tags/"wfo-$version" &&
+  cd "wfo-$version" &&
+  autoconf
+) &&
+(
+  cd $tmpdir &&
+  tar cvf - "wfo-$version"
+) > "wfo-$version".tar &&
+gzip -9 "wfo-$version".tar &&
+rm -rf $tmpdir
