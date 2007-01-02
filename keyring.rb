6d400e4a97c66b3f0137852221c2bba35f185b61
Tanaka Akira <akr@fsij.org>
2007-01-02 02:15:46 +0000

    add copyright year 2007.
    git-svn-id: svn://svn.a-k-r.org/akr/wfo/trunk@244 0ecacc03-ac1b-0410-941d-e442c79421a6

--- keyring.rb
+++ keyring.rb
@@ -1,6 +1,6 @@
 # keyring.rb - password storage library
 #
-# Copyright (C) 2006 Tanaka Akira  <akr@fsij.org>
+# Copyright (C) 2006,2007 Tanaka Akira  <akr@fsij.org>
 # 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
