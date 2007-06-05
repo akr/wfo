9f6962299a3fee46a678405a26a75d5f0eccc781
Tanaka Akira <akr@fsij.org>
2007-06-05 22:56:54 +0000

    * wfo/cookie.rb (WFO::Cookie#match?): http://example.org didn't match   if "Path=/".
    git-svn-id: svn://svn.a-k-r.org/akr/wfo/trunk@668 0ecacc03-ac1b-0410-941d-e442c79421a6

--- wfo/cookie.rb
+++ wfo/cookie.rb
@@ -84,7 +84,7 @@ class WFO::Cookie
 
   def match?(uri)
     return false if @domain_pat !~ uri.host
-    return false if @path_pat !~ uri.path
+    return false if @path_pat !~ uri.request_uri
     return false if @pairs.find {|k, v| /\Asecure\z/i =~ k } && uri.scheme != 'https'
     true
   end
